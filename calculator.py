7ccf3042faaff863c7c30402d17595bbed085f59
Tyler Pettigrew <pettigrt@oregonstate.edu>
2021-03-09 12:11:01 -0800

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#Basic calculator functions, no main function included
+
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a / b
